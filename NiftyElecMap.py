f1195d823075ce2ce80012a78ebd0c7ca10bef3e
Ankit Khambhati <akhambhati@gmail.com>
2012-03-08 12:20:50 -0500

    Created feature_extraction instance to simulate real-time feature extraction. Added Right Key functionality to train feature extractor and then displayon triangulated surface.

--- NiftyElecMap.py
+++ NiftyElecMap.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 """
 A tool for plotting and mapping representative electrode channels on cortical
 surface renderings.
@@ -7,7 +7,7 @@ surface renderings.
 
 # Scientific Libraries
 import math
-import numpy
+import numpy as np
 
 # I/O Handling
 from Volume.Formats import NIfTI
@@ -37,8 +37,14 @@ from electrodes import CTElectrode
 import wx
 
 
+# Simulation Libraries
+import feature_extraction as RTSim
+import time
+
+
 class ElectrodeMappingInteractor(wxVTKRenderWindowInteractor):
-    def __init__(self, parent, brain_data, elec_map_fname, elec_ct_data=None):
+    def __init__(self, parent, brain_data,\
+            simData, elec_map_fname, elec_ct_data=None):
 
         #----------------------------------------------------------------------
         # Renderer and Interactor
@@ -52,15 +58,19 @@ class ElectrodeMappingInteractor(wxVTKRenderWindowInteractor):
         myPatient = Cortex(brain_data)
         ren.AddViewProp(myPatient)
 
-        #---------------------------------------------------------------------
+        #----------------------------------------------------------------------
+        # Setup Simulation Module
+        simStream = simData.SlidingWindow()
+
+        #----------------------------------------------------------------------
         # Check and render segmented electrode CT surface
         print elec_ct_data
         if elec_ct_data is not None:
             myElectrodeCT = CTElectrode(elec_ct_data)
             #ren.AddViewProp(myElectrodeCT)
-            ren.AddViewProp(myElectrodeCT.grid)
+            #ren.AddViewProp(myElectrodeCT.grid)
             ren.AddViewProp(myElectrodeCT.triangulation)
-        else:
+            #ren.AddViewProp(myElectrodeCT.cbr)
             print "No electrode CT data specified"
 
         # Use the trackball camera for interaction
@@ -152,15 +162,24 @@ class ElectrodeMappingInteractor(wxVTKRenderWindowInteractor):
 
             # Press 'UP ARROW' to increase opacity of cortex surface
             if key_code == wx.WXK_UP:
-                #myPatient.SetOpacityUp()
-                for i in range(100):
-                    myElectrodeCT.UpdateGridSurface()
-                    self.Render()
-
+                myPatient.SetOpacityUp()
 
             # Press 'DOWN ARROW' to decrease opacity of cortex surface
             if key_code == wx.WXK_DOWN:
                 myPatient.SetOpacityDown()
+
+            # Press 'RIGHT ARROW' to start the real time simulation
+            if key_code == wx.WXK_RIGHT:
+                startTime = time.clock()
+                tic = time.clock()
+                toc = time.clock()
+                while(1):
+                    time.sleep(0.02)
+                    tic = time.clock()
+                    myElectrodeCT.UpdateGridSurface(simStream.next())
+                    self.Render()
+                    toc = time.clock()
+                    #print "Update Time: %3.2f msec || Elapsed Time: %3f sec" % (100*(toc-tic), time.clock()-startTime)
 
         parent.Bind(wx.EVT_KEY_UP, OnKeyPress)
 
@@ -200,8 +219,34 @@ if __name__ == '__main__':
         elect_data = None
         print 'Could not import electrode NIfTI!'
 
+
+    # Real time EEG simulatio
+    fname = "/home/akhambhati/Litt_Fourier/ankk/Neuralynx_RT Project/Mayo Data/NEO_SZ_01/NEO_SZ_01_Grid_slice_003.mat"
+
+    """
+    20th order Cauer filter, 100 - 500 Hz bandpass, 
+    65dB minimum lower/upper stopband attenuation
+    0.5dB maximum pass band ripple
+    25 Hz lower/upper transition width
+    """
+    import scipy.signal as sigp
+    filt_num, filt_den = sigp.iirdesign(\
+            wp = [100./(2713./2), 500./(2713./2)],\
+            ws= [75./(2713./2), 525./(2713./2)],\
+            gstop= 60, gpass=0.5, ftype='ellip')
+    HFOFeat = lambda winSignal: np.sqrt(np.mean(\
+            sigp.filtfilt(filt_num, filt_den, winSignal)**2))
+    LLFeat = lambda winSignal: np.sqrt(np.mean(np.diff(winSignal)**2))
+
+
+    temporalGrid = RTSim.EEGElectrode(fname,\
+            winSize=0.1, winShift=0.05, trainTime=60,\
+            feat=LLFeat)
+
+
     canvas = ElectrodeMappingInteractor(frame,\
                                             brain_data,\
+                                            temporalGrid,\
                                             electrode_map_fname,\
                                             elect_data)
 
